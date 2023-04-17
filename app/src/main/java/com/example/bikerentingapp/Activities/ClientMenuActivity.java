982709ade472d70e3b4188c2747fad740d5f8aef
Szymon Czech <szymonpoczta@interia.pl>
2023-04-17 21:41:32 +0200

    Adden user navigation menu.

--- app/src/main/java/com/example/bikerentingapp/Activities/ClientMenuActivity.java
+++ app/src/main/java/com/example/bikerentingapp/Activities/ClientMenuActivity.java
@@ -1,12 +1,16 @@
 package com.example.bikerentingapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.bikerentingapp.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class ClientMenuActivity extends AppCompatActivity {
 
@@ -14,11 +18,20 @@ public class ClientMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_menu_activity);
+
+        //Operating on navigation view
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
     }
 
     public void openMapActivity(View view){
 
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
+    }
+
+    public void openNavigationMenu(View view){
+        final DrawerLayout navigation = findViewById(R.id.drawerLayout);
+        navigation.openDrawer(GravityCompat.START);
     }
 }
